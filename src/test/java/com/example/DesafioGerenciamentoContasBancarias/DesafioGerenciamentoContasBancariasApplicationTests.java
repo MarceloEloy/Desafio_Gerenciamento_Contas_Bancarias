59b741689a61151854204a181765ef322335f49c
MarceloEloy <marcosfaro72@gmail.com>
2026-09-19 20:11:13 -0300

    primeiro commit do projeto

---
+++ src/test/java/com/example/DesafioGerenciamentoContasBancarias/DesafioGerenciamentoContasBancariasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DesafioGerenciamentoContasBancarias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioGerenciamentoContasBancariasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
